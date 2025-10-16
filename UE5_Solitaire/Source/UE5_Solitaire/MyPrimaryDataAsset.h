0fa64f1f360244ab3bf3cd08de0404e3fb54239e
xuke <825126369@qq.com>
2025-10-17 00:12:03 +0800

    TODO：资源管理器

---
+++ UE5_Solitaire/Source/UE5_Solitaire/MyPrimaryDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MyPrimaryDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class UE5_SOLITAIRE_API UMyPrimaryDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
